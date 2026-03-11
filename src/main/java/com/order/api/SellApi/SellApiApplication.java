ac92a837f9660e6b61cbabb33f8422d44f7f3b2c
Gabrielcamargos28 <gabrielcamargosptc@gmail.com>
2026-03-11 20:30:14 -0300

    feat: add rest template

--- src/main/java/com/order/api/SellApi/SellApiApplication.java
+++ src/main/java/com/order/api/SellApi/SellApiApplication.java
@@ -2,12 +2,19 @@ package com.order.api.SellApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SellApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SellApiApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
